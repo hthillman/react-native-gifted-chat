4a494eae0829ca2dce1a7dd6856012c2603b9a7e
Hunter Hillman <35902552+hthillman@users.noreply.github.com>
2019-12-31 10:37:17 -0500

    Update Composer.tsx

--- src/Composer.tsx
+++ src/Composer.tsx
@@ -1,103 +1,32 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import { Platform, StyleSheet, TextInput, TextInputProps } from 'react-native'
-import { MIN_COMPOSER_HEIGHT, DEFAULT_PLACEHOLDER } from './Constant'
-import Color from './Color'
+/* eslint no-use-before-define: ["error", { "variables": false }] */
 
-const styles = StyleSheet.create({
-  textInput: {
-    flex: 1,
-    marginLeft: 10,
-    fontSize: 16,
-    lineHeight: 16,
-    ...Platform.select({
-      web: {
-        paddingTop: 6,
-        paddingLeft: 4,
-      },
-    }),
-    marginTop: Platform.select({
-      ios: 6,
-      android: 0,
-      web: 6,
-    }),
-    marginBottom: Platform.select({
-      ios: 5,
-      android: 3,
-      web: 4,
-    }),
-  },
-})
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Platform, StyleSheet, TextInput } from 'react-native';
 
-export interface ComposerProps {
-  composerHeight?: number
-  text?: string
-  placeholder?: string
-  placeholderTextColor?: string
-  textInputProps?: Partial<TextInputProps>
-  textInputStyle?: TextInputProps['style']
-  textInputAutoFocus?: boolean
-  keyboardAppearance?: TextInputProps['keyboardAppearance']
-  multiline?: boolean
-  disableComposer?: boolean
-  onTextChanged?(text: string): void
-  onInputSizeChanged?(contentSize: { width: number; height: number }): void
-}
+import { MIN_COMPOSER_HEIGHT, DEFAULT_PLACEHOLDER } from './Constant';
+import Color from './Color';
 
-export default class Composer extends React.Component<ComposerProps> {
-  static defaultProps = {
-    composerHeight: MIN_COMPOSER_HEIGHT,
-    text: '',
-    placeholderTextColor: Color.defaultColor,
-    placeholder: DEFAULT_PLACEHOLDER,
-    textInputProps: null,
-    multiline: true,
-    disableComposer: false,
-    textInputStyle: {},
-    textInputAutoFocus: false,
-    keyboardAppearance: 'default',
-    onTextChanged: () => {},
-    onInputSizeChanged: () => {},
-  }
+export default class Composer extends React.Component {
 
-  static propTypes = {
-    composerHeight: PropTypes.number,
-    text: PropTypes.string,
-    placeholder: PropTypes.string,
-    placeholderTextColor: PropTypes.string,
-    textInputProps: PropTypes.object,
-    onTextChanged: PropTypes.func,
-    onInputSizeChanged: PropTypes.func,
-    multiline: PropTypes.bool,
-    disableComposer: PropTypes.bool,
-    textInputStyle: PropTypes.any,
-    textInputAutoFocus: PropTypes.bool,
-    keyboardAppearance: PropTypes.string,
-  }
-
-  contentSize?: { width: number; height: number } = undefined
-
-  onContentSizeChange = (e: any) => {
-    const { contentSize } = e.nativeEvent
+  onContentSizeChange(e) {
+    const { contentSize } = e.nativeEvent;
 
     // Support earlier versions of React Native on Android.
-    if (!contentSize) {
-      return
-    }
+    if (!contentSize) return;
 
     if (
       !this.contentSize ||
-      (this.contentSize &&
-        (this.contentSize.width !== contentSize.width ||
-          this.contentSize.height !== contentSize.height))
+      this.contentSize.width !== contentSize.width ||
+      this.contentSize.height !== contentSize.height
     ) {
-      this.contentSize = contentSize
-      this.props.onInputSizeChanged!(this.contentSize!)
+      this.contentSize = contentSize;
+      this.props.onInputSizeChanged(this.contentSize);
     }
   }
 
-  onChangeText = (text: string) => {
-    this.props.onTextChanged!(text)
+  onChangeText(text) {
+    this.props.onTextChanged(text);
   }
 
   render() {
@@ -109,31 +38,66 @@ export default class Composer extends React.Component<ComposerProps> {
         placeholder={this.props.placeholder}
         placeholderTextColor={this.props.placeholderTextColor}
         multiline={this.props.multiline}
-        editable={!(this.props.disableComposer)}
-        onChange={this.onContentSizeChange}
-        onContentSizeChange={this.onContentSizeChange}
-        onChangeText={this.onChangeText}
-        style={[
-          styles.textInput,
-          this.props.textInputStyle,
-          {
-            height: this.props.composerHeight,
-            ...Platform.select({
-              web: {
-                outlineWidth: 0,
-                outlineColor: 'transparent',
-                outlineOffset: 0,
-              },
-            }),
-          },
-        ]}
+        onChange={(e) => this.onContentSizeChange(e)}
+        onContentSizeChange={(e) => this.onContentSizeChange(e)}
+        onChangeText={(text) => this.onChangeText(text)}
+        style={[styles.textInput, this.props.textInputStyle, { height: this.props.composerHeight }]}
         autoFocus={this.props.textInputAutoFocus}
         value={this.props.text}
         enablesReturnKeyAutomatically
-        underlineColorAndroid='transparent'
+        underlineColorAndroid="transparent"
         keyboardAppearance={this.props.keyboardAppearance}
         {...this.props.textInputProps}
       />
-    )
+    );
   }
+
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    flex: 1,
+    alignItems:"flex-start",
+    justifyContent:"flex-start",
+    textAlignVertical:"top",
+    marginLeft: 10,
+    fontSize: 16,
+    lineHeight: 16,
+    marginTop: Platform.select({
+      ios: 6,
+      android: 10,
+    }),
+    marginBottom: Platform.select({
+      ios: 5,
+      android: 3,
+    }),
+  },
+});
+
+Composer.defaultProps = {
+  composerHeight: MIN_COMPOSER_HEIGHT,
+  text: '',
+  placeholderTextColor: Color.defaultProps,
+  placeholder: DEFAULT_PLACEHOLDER,
+  textInputProps: null,
+  multiline: true,
+  textInputStyle: {},
+  textInputAutoFocus: false,
+  keyboardAppearance: 'default',
+  onTextChanged: () => {},
+  onInputSizeChanged: () => {},
+};
+
+Composer.propTypes = {
+  composerHeight: PropTypes.number,
+  text: PropTypes.string,
+  placeholder: PropTypes.string,
+  placeholderTextColor: PropTypes.string,
+  textInputProps: PropTypes.object,
+  onTextChanged: PropTypes.func,
+  onInputSizeChanged: PropTypes.func,
+  multiline: PropTypes.bool,
+  textInputStyle: TextInput.propTypes.style,
+  textInputAutoFocus: PropTypes.bool,
+  keyboardAppearance: PropTypes.string,
+};
